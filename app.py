881eaf7c8449b6677667de523ec63d650fd989ed
rantad584 <rantad584@github.com>
2024-05-07 02:58:29 +0000

    Add sample output

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 
-from flask import Flask
+from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
+# Accept post requests to this route
+@app.route('/upload', methods=['POST'])
 def main():
-    return 'Hello, Flask!'
+    if 'imageFile' not in request.files:
+        return 'No file part', 400
+
+    file = request.files['imageFile']
+
+    if file.filename == '':
+        return 'No selected file', 400
+
+    return json.dumps(
+        {
+            'class': 'glass',
+            'confidence:': '80%',
+            'filename': file.filename
+        }
+    , indent=4)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
