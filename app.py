371e46783ab73d0b795cba87e753b0c2b925b1f4
rantad584 <rantad584@github.com>
2024-05-10 03:04:57 +0000

    Set up classifcation model

--- app.py
+++ app.py
@@ -1,23 +1,55 @@
 
 from flask import Flask, request
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 import json
+from base64 import b64decode
+from io import BytesIO
+from PIL import Image
 
 app = Flask(__name__)
+model = keras.models.load_model('recycling_model.keras')
+class_names = [
+ 'Carton',
+ 'Organic Waste',
+ 'Wood',
+ 'battery',
+ 'cardboard',
+ 'clothes',
+ 'glass',
+ 'metal',
+ 'paper',
+ 'plastic',
+ 'shoes',
+ 'trash']
+
+def predict_category(image_string):
+    try:
+        image = Image.open(BytesIO(b64decode(image_string))) # decode base64 encoded image and create up PIL instance
+
+        # Resize and preprocess the image
+        image = tf.expand_dims(keras.utils.img_to_array(image.resize((256, 256))), axis=0)
+
+        prediction = model.predict(image)
+        score = tf.nn.softmax(prediction[0])
+
+        return json.dumps(
+            {
+                'category': class_names[np.argmax(score)],
+                'confidence:': f'{np.max(score) * 100}%'
+            }
+        , indent=4)
+
+    except Exception as e:
+        print(f'Exception {e}')
 
 # Accept post requests to this route
 @app.route('/upload', methods=['POST'])
 def main():
     obj = request.get_json() # get request and convert to python object
 
-    file = obj['image']
-
-    return json.dumps(
-        {
-            'class': 'glass',
-            'confidence:': '80%',
-            'filename': file
-        }
-    , indent=4)
+    return predict_category(obj['image'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
