b45c84908356caa588ee391bfa4f4c00c1c6357e
rantad584 <rantad584@github.com>
2024-08-04 14:26:31 +1000

    Update formatting

--- app.py
+++ app.py
@@ -1,36 +1,40 @@
-
-from flask import Flask, request
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
 import json
+import numpy as np
 from base64 import b64decode
 from io import BytesIO
 from PIL import Image
+from flask import Flask, request
+import tensorflow as tf
+from tensorflow import keras
+
 
 app = Flask(__name__)
-model = keras.models.load_model('recycling_model.keras')
+model = keras.models.load_model("recycling_model.keras")
 class_names = [
- 'Carton',
- 'Organic Waste',
- 'Wood',
- 'battery',
- 'cardboard',
- 'clothes',
- 'glass',
- 'metal',
- 'paper',
- 'plastic',
- 'shoes',
- 'trash']
+    "Carton",
+    "Organic Waste",
+    "Wood",
+    "battery",
+    "cardboard",
+    "clothes",
+    "glass",
+    "metal",
+    "paper",
+    "plastic",
+    "shoes",
+    "trash",
+]
+
 
 def predict_category(image_string):
     try:
         # decode base64 encoded image and create up PIL instance
-        image = Image.open(BytesIO(b64decode(image_string))).convert('RGB')
+        image = Image.open(BytesIO(b64decode(image_string))).convert("RGB")
 
         # Resize and preprocess the image
-        image = tf.expand_dims(keras.utils.img_to_array(image.resize((256, 256))), axis=0)
+        image = tf.expand_dims(
+            keras.utils.img_to_array(image.resize((256, 256))), axis=0
+        )
 
         prediction = model.predict(image)
         scores = tf.nn.softmax(prediction[0])
@@ -44,14 +48,15 @@ def predict_category(image_string):
         return json.dumps(result, indent=4)
 
     except Exception as e:
-        print(f'Exception {e}')
+        print(f"Exception {e}")
+
 
 # Accept post requests to this route
-@app.route('/', methods=['POST'])
+@app.route("/", methods=["POST"])
 def main():
-    obj = request.get_json() # get request and convert to python object
+    obj = request.get_json()  # get request and convert to python object
+    return predict_category(obj["image"])
 
-    return predict_category(obj['image'])
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
